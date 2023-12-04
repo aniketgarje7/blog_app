438026aef2b0d742ed3a31a2b7191f844aede4fb
aniketgarje7 <garjeaniket7@gmail.com>
2023-12-04 11:46:26 +0530

    added autoresize for textarea

--- frontend/src/components/Root/Blog/BlogCreate.js
+++ frontend/src/components/Root/Blog/BlogCreate.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import ButtonLoader from "../../Elements/ButtonLoader";
 import {useDispatch} from 'react-redux';
 import { createBlog, getBlogs } from "../../../store/slices/BlogSlice";
+import autosize from 'autosize';
 
 const BlogCreate = () => {
   const [title, setTitle] = useState("");
@@ -26,6 +27,11 @@ const BlogCreate = () => {
       setIsLoading(false);
     })
   };
+  const adjustHeight=(element)=>{
+    const el = element.target;
+    // el.style.height = (el.scrollHeight > el.clientHeight) && (el.scrollHeight)+10+"px";
+    autosize(el);
+  }
   return (
     <div className="mb-2">
       <Form onSubmit={handleSubmit}>
@@ -33,7 +39,7 @@ const BlogCreate = () => {
           <Form.Control type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} value={title} />
         </Form.Group>
         <Form.Group className="mb-2 px-1" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={3} placeholder="Write something" onChange={(e) => setTextBody(e.target.value)} value={textBody} />
+          <Form.Control as="textarea" rows={3} placeholder="Write something" onChange={(e) => setTextBody(e.target.value)} value={textBody} onKeyUp={adjustHeight} className="textarea_blog"/>
         </Form.Group>
         <div className="text-end">
           <Button type="submit" disabled={isLoading}>
